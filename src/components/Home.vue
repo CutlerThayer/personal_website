56ac43d484e3e62acbb05013315a2eb2dd8811b7
Cutler Thayer <cutlerthayer@gmail.com>
2021-08-08 15:33:42 -0500

    Very basic home page component

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-container>
+        <p>Start of home page</p>
+    </v-container>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+
+})
+</script>
