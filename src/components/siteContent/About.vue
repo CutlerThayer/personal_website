9ac0892c52f9f4cf1823e3b4928efc39d6132819
Cutler <cutlerthayer@gmail.com>
2021-09-20 19:58:10 -0500

    Added content to about

--- src/components/siteContent/About.vue
+++ src/components/siteContent/About.vue
@@ -1,6 +1,10 @@
 <template>
     <v-container>
-        <h1>Continue</h1>
+        <h1>
+            My name is Cutler Thayer, and I am currently a student at Iowa State University studying Computer Engineering. 
+            I have a passion for development of both hardware and software, and I would like to use the skills I have learned 
+            to make the world a better place.
+        </h1>
     </v-container>
 </template>
 
@@ -14,6 +18,6 @@ export default defineComponent({
 
 <style scoped>
 * {
-    color: blue;
+    color: black;
 }
 </style>
